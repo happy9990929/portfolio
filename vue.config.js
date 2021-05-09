cb28774812303df76856ac8b0944fcf0ff7d1ac8
lala <lala9990929@gmail.com>
2021-05-09 23:11:14 +0800

    'db'

--- vue.config.js
+++ vue.config.js
@@ -2,22 +2,6 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/portfolio/'
     : '/',
-  devServer: {
-    open: true,
-    host: 'localhost',
-    port: '8080',
-    https: false,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // 路径指向本地主机地址及端口号
-        ws: true,
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '/data', // 路径转发代理
-        },
-      },
-    },
-  },
   pages: {
     index: {
       // entry for the page
